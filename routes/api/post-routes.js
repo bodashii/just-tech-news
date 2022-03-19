124ab2f602e28c6239aefe49fdc06fd908d5fd73
maddnty <nzkb24@gmail.com>
2022-03-18 23:37:36 -0500

    added routes for comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,14 @@
-const sequelize = require('../../config/connection');
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const sequelize = require('../../config/connection');
+const { Post, User, Vote, Comment } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
         // Query configuration
+        
+        order: [['created_at', 'DESC']],
         attributes: [
             'id',
             'post_url',
@@ -14,8 +16,21 @@ router.get('/', (req, res) => {
             'created_at',
             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
-        order: [['created_at', 'DESC']],
         include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'post_id',
+                    'user_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
