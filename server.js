34580e5d3c8c8ce7e201d4ac38ea8d42c8443240
maddnty <nzkb24@gmail.com>
2022-06-22 11:47:17 -0500

    updated gitignore

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const path = require('path');
+
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
 app.use(routes);
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
