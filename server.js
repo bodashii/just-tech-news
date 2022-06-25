8c7d87d8388d64a33a7139effd9c215054f64c14
maddnty <nzkb24@gmail.com>
2022-06-24 20:01:06 -0500

    changes

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
-const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
-const path = require('path');
+const path = require("path");
+const express = require("express");
+
+const exphbs = require("express-handlebars");
 
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
+const sequelize = require("./config/connection");
+
+const hbs = exphbs.create({});
+
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 // turn on routes
-app.use(routes);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers'));
+app.use(express.static(path.join(__dirname, "public")));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now Listening'));
+  app.listen(PORT, () => console.log("Now Listening"));
 });
